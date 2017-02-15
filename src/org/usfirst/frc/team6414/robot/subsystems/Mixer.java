de6ddbfa73ad126d4e5f7b1cfbe003b3b0fda88a
willson0v0 <willsonlgz@gmail.com>
2017-02-15 20:08:33 +0800

    better mixer

--- src/org/usfirst/frc/team6414/robot/subsystems/Mixer.java
+++ src/org/usfirst/frc/team6414/robot/subsystems/Mixer.java
@@ -6,6 +6,8 @@ import org.usfirst.frc.team6414.robot.Robot;
 import org.usfirst.frc.team6414.robot.RobotMap;
 import org.usfirst.frc.team6414.robot.commands.Mix;
 
+
+
 /**
  * Created by willson on 2017/2/14.
  *
@@ -15,7 +17,27 @@ import org.usfirst.frc.team6414.robot.commands.Mix;
 public class Mixer extends Subsystem {
 
     private CANTalon mixer = new CANTalon(RobotMap.MIXER_MOTOR);
-    private boolean isFwd = true;
+    private State state = State.STOP;
+    enum State{
+        FORWARD,
+        STOP,
+        BACKWARD;
+        public State fwdPressed(){
+            if (this == FORWARD) {
+                return STOP;
+            }else{
+                return FORWARD;
+            }
+        }
+        public State bwdPressed(){
+            if (this == BACKWARD) {
+                return STOP;
+            }else{
+                return BACKWARD;
+            }
+        }
+    }
+
 
     public Mixer(){
         super();
@@ -23,16 +45,27 @@ public class Mixer extends Subsystem {
     }
 
     public void mix(){
-        if(isFwd){
-            mixer.set(RobotMap.MIXER_DEF);
-        }else{
-            mixer.set(-RobotMap.MIXER_DEF);
+//        if(isFwd){
+//            mixer.set(RobotMap.MIXER_DEF);
+//        }else{
+//            mixer.set(-RobotMap.MIXER_DEF);
+//        }
+        switch (state){
+            case FORWARD:
+                mixer.set(RobotMap.MIXER_DEF);
+                break;
+            case BACKWARD:
+                mixer.set(-RobotMap.MIXER_DEF);
+                break;
+            case STOP:
+            default:
+                mixer.set(0);
         }
         if(Robot.oi.getButSt(RobotMap.MIXER_FWD)){
-            isFwd = true;
+            state=state.fwdPressed();
         }
         if(Robot.oi.getButSt(RobotMap.MIXER_BWD)){
-            isFwd = false;
+            state=state.bwdPressed();
         }
     }
 
